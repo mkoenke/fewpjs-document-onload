46dcc67060b0adf41a3a2e79738fe54531ac63c6
mkoenke <mkoenke@sas.upenn.edu>
2020-11-16 18:15:19 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let id = document.querySelector("#text");
+    id.textContent = "This is really cool!"
+  });
